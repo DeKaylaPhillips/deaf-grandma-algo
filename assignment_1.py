aceea22487787e9587641d2ed4325a26e391b471
DeKayla Phillips <dekaylanicole@gmail.com>
2024-11-16 14:25:11 -0500

    Refactor to encapsulate and move global object into function

--- assignment_1.py
+++ assignment_1.py
@@ -1,30 +1,29 @@
-grandmas_response = {
-    "empty_input": "WHAT?!",
-    "lowercase_input": "SPEAK UP, KID!",
-    "uppercase_input": "NO, NOT SINCE 1946!",
-    "first_goodbye": "LEAVING SO SOON?",
-    "second_goodbye": "LATER, SKATER!",
-}
-
-
 def deaf_grandma() -> None:
+    responses = {
+        "empty_input": "WHAT?!",
+        "lowercase_input": "SPEAK UP, KID!",
+        "uppercase_input": "NO, NOT SINCE 1946!",
+        "first_goodbye": "LEAVING SO SOON?",
+        "second_goodbye": "LATER, SKATER!",
+    }
+    
     goodbye_count = 0
 
     while True:
         user_input = input("Speak to grandma: \n")
 
         if not user_input:
-            print(grandmas_response["empty_input"], "\n")
+            print(responses["empty_input"], "\n")
         elif user_input == "GOODBYE!":
             goodbye_count += 1
             if goodbye_count == 1:
-                print(grandmas_response["first_goodbye"], "\n")
+                print(responses["first_goodbye"], "\n")
             elif goodbye_count == 2:
-                print(grandmas_response["second_goodbye"], "\n")
+                print(responses["second_goodbye"], "\n")
                 break
         elif any(char.islower() for char in user_input):
-            print(grandmas_response["lowercase_input"], "\n")
+            print(responses["lowercase_input"], "\n")
         elif user_input.isupper():
-            print(grandmas_response["uppercase_input"], "\n")
+            print(responses["uppercase_input"], "\n")
 
 deaf_grandma()
